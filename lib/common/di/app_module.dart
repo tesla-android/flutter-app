570f36758d3fada0e1c7a41e11ab5908d3145680
Michał Gapiński <mike@gapinski.eu>
2022-11-08 01:18:54 +0100

    Use window.location.hostname in urls for all calls to services. Closes #8

--- lib/common/di/app_module.dart
+++ lib/common/di/app_module.dart
@@ -2,6 +2,7 @@ import 'package:flavor/flavor.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart' hide Environment;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:html';
 
 @module
 abstract class AppModule {
@@ -10,12 +11,22 @@ abstract class AppModule {
         Environment.production,
         color: Colors.red,
         properties: {
-          'touchscreenWebSocket': 'ws://9.9.0.1:9999',
-          'videoLayerUrl' : 'http://9.9.0.1:9090/',
+          'touchscreenWebSocket': 'ws://' + (window.location.hostname ?? "9.9.0.1") + ':9999',
+          'videoLayerUrl' : 'http://' + (window.location.hostname ?? "9.9.0.1") + ':9090/',
           'virtualDisplayWidth': 1034,
           'virtualDisplayHeight': 788,
         },
       );
+  // Flavor get provideFlavor => Flavor.create(
+  //       Environment.dev,
+  //       color: Colors.green,
+  //       properties: {
+  //         'touchscreenWebSocket': 'ws://9.9.0.1:9999',
+  //         'videoLayerUrl' : 'http://9.9.0.1:9090/',
+  //         'virtualDisplayWidth': 1034,
+  //         'virtualDisplayHeight': 788,
+  //       },
+  //     );
 
   @singleton
   @preResolve
