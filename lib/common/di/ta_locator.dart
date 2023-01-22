53807290721f423b3afcf3348ec18630b8663f87
Michał Gapiński <mike@gapinski.eu>
2023-01-22 19:37:57 +0100

    Injectable Fix

--- lib/common/di/ta_locator.dart
+++ lib/common/di/ta_locator.dart
@@ -1,9 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-
-import 'ta_locator.config.dart';
+import 'package:tesla_android/common/di/ta_locator.config.dart';
 
 final getIt = GetIt.instance;
 
 @InjectableInit()
-Future configureTADependencies() => $initGetIt(getIt);
+Future configureTADependencies() => getIt.init();
