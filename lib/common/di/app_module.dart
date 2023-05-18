92a1c208fc55ef10ed001c663b7206d9e0cae100
Michał Gapiński <mike@gapinski.eu>
2023-05-18 19:11:32 +0200

    Replace 9.9.0.1 with 100.64.255.1(CGNAT)

--- lib/common/di/app_module.dart
+++ lib/common/di/app_module.dart
@@ -1,27 +1,30 @@
 import 'dart:html';
 
+import 'package:dio/dio.dart';
 import 'package:flavor/flavor.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart' hide Environment;
 import 'package:shared_preferences/shared_preferences.dart';
 
 const bool _enableIpOverride = false;
-const String _ipOverride = '192.168.0.xxx';
+const String _ipOverride = '100.64.255.1';
 
 @module
 abstract class AppModule {
   @singleton
   Flavor get provideFlavor {
-    final ipAddress = _enableIpOverride ? _ipOverride : (window.location.hostname ?? "9.9.0.1");
+    final ipAddress = _enableIpOverride
+        ? _ipOverride
+        : (window.location.hostname ?? "100.64.255.1");
     return Flavor.create(
       _enableIpOverride ? Environment.dev : Environment.production,
       color: _enableIpOverride ? Colors.green : Colors.red,
       properties: {
         'touchscreenWebSocket': 'ws://$ipAddress:9999',
-        'displayWebSocket' : 'ws://$ipAddress:9090',
-        'audioWebSocket' : 'ws://$ipAddress:8080',
-        'configurationApiBaseUrl' : 'http://$ipAddress:8081',
-        'connectivityCheck' : 'http://$ipAddress/online/connectivity_check.txt',
+        'displayWebSocket': 'ws://$ipAddress:9090',
+        'audioWebSocket': 'ws://$ipAddress:8080',
+        'configurationApiBaseUrl': 'http://$ipAddress:8081',
+        'connectivityCheck': 'http://$ipAddress/online/connectivity_check.txt',
         'virtualDisplayWidth': 1088,
         'virtualDisplayHeight': 832,
       },
@@ -30,7 +33,8 @@ abstract class AppModule {
 
   @singleton
   @preResolve
-  Future<SharedPreferences> get sharedPreferences => SharedPreferences.getInstance();
+  Future<SharedPreferences> get sharedPreferences =>
+      SharedPreferences.getInstance();
 
   @lazySingleton
   GlobalKey<NavigatorState> get navigatorKey => GlobalKey<NavigatorState>();
