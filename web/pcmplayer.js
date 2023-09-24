c3729773924ba92c6da720ef674022f58c1a0ddd
david@vanronk.net <david@vanronk.net>
2023-09-24 11:22:48 -0500

    optimize pcmplayer to remove unnecessary data conversions
    Since we always use Float32, we have no need to do conversions. This yields improvements because the conversion function was rebuilding the buffer even though it was formatted correctly.

--- web/pcmplayer.js
+++ web/pcmplayer.js
@@ -1,8 +1,9 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.PCMPlayer = factory());
-})(this, (function () { 'use strict';
+    typeof define === 'function' && define.amd ? define(factory) :
+      (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.PCMPlayer = factory());
+})(this, (function () {
+  'use strict';
 
   class PCMPlayer {
     constructor(option) {
@@ -20,30 +21,15 @@
       this.option = Object.assign({}, defaultOption, option); // 实例最终配置参数
       this.samples = new Float32Array(); // 样本存放区域
       this.interval = setInterval(this.flush.bind(this), this.option.flushTime);
-      this.convertValue = this.getConvertValue();
+
       this.typedArray = this.getTypedArray();
       this.initAudioContext();
     }
 
-    getConvertValue() {
-      // 根据传入的目标编码位数
-      // 选定转换数据所需要的基本值
-      const inputCodecs = {
-        'Int8': 128,
-        'Int16': 32768,
-        'Int32': 2147483648,
-        'Float32': 1
-      };
-      if (!inputCodecs[this.option.inputCodec]) {
-        throw new Error('wrong codec.please input one of these codecs:Int8,Int16,Int32,Float32')
-      }
-      return inputCodecs[this.option.inputCodec]
-    }
+
 
     getTypedArray() {
-      // 根据传入的目标编码位数
-      // 选定前端的所需要的保存的二进制数据格式
-      // 完整TypedArray请看文档
+      // Select the binary data format that needs to be saved by the front end according to the target encoding bit number. See the complete TypedArray in the documentation
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray
       const typedArrays = {
         'Int8': Int8Array,
@@ -58,9 +44,9 @@
     }
 
     initAudioContext() {
-      // 初始化音频上下文的东西
+      // Initialize the audio context
       this.audioCtx = new (window.parent.AudioContext || window.parent.webkitAudioContext)();
-      // 控制音量的 GainNode
+      // GainNode that controls the volume
       // https://developer.mozilla.org/en-US/docs/Web/API/BaseAudioContext/createGain
       this.gainNode = this.audioCtx.createGain();
       this.gainNode.gain.value = 1.0;
@@ -69,15 +55,14 @@
     }
 
     static isTypedArray(data) {
-      // 检测输入的数据是否为 TypedArray 类型或 ArrayBuffer 类型
+      //Check whether the input data is of the TypedArray type or ArrayBuffer type
       return (data.byteLength && data.buffer && data.buffer.constructor == ArrayBuffer) || data.constructor == ArrayBuffer;
     }
 
     isSupported(data) {
-      // 数据类型是否支持
-      // 目前支持 ArrayBuffer 或者 TypedArray
+      // Currently, the code supports ArrayBuffer or TypedArray
       if (!PCMPlayer.isTypedArray(data)) {
-        throw new Error('请传入ArrayBuffer或者任意TypedArray')
+        throw new Error('Not a ArrayBuffer or TypedArray')
       }
       return true
     }
@@ -85,40 +70,28 @@
     feed(data) {
       this.isSupported(data);
 
-      // 获取格式化后的buffer
-      data = this.getFormatedValue(data);
-      // 开始拷贝buffer数据
-      // 新建一个Float32Array的空间
+      // getting the formatted buffer.
+      if (data.constructor == ArrayBuffer) {
+        data = new Float32Array(data);
+      } else {
+        data = new Float32Array(data.buffer);
+      }
+      // the code is starting to copy data from a buffer. It then creates a new Float32Array to store the copied data.
       const tmp = new Float32Array(this.samples.length + data.length);
-      // console.log(data, this.samples, this.samples.length)
-      // 复制当前的实例的buffer值（历史buff)
-      // 从头（0）开始复制
+
       tmp.set(this.samples, 0);
-      // 复制传入的新数据
-      // 从历史buff位置开始
+      // Copy the new data passed in, starting from the historical buffer position
       tmp.set(data, this.samples.length);
-      // 将新的完整buff数据赋值给samples
-      // interval定时器也会从samples里面播放数据
+      // code is assigning a new buffer of data to the samples variable. The interval timer will then play the data from the samples variable.
       this.samples = tmp;
     }
 
-    getFormatedValue(data) {
+    getFormattedValue(data) {
       if (data.constructor == ArrayBuffer) {
-        data = new this.typedArray(data);
+        return new Float32Array(data);
       } else {
-        data = new this.typedArray(data.buffer);
+        return new Float32Array(data.buffer);
       }
-
-      let float32 = new Float32Array(data.length);
-
-      for (let i = 0; i < data.length; i++) {
-        // buffer 缓冲区的数据，需要是IEEE754 里32位的线性PCM，范围从-1到+1
-        // 所以对数据进行除法
-        // 除以对应的位数范围，得到-1到+1的数据
-        // float32[i] = data[i] / 0x8000;
-        float32[i] = data[i] / this.convertValue;
-      }
-      return float32
     }
 
     volume(volume) {
@@ -136,14 +109,14 @@
 
     flush() {
       if (!this.samples.length) return
-      var bufferSource = this.audioCtx.createBufferSource();
+      let bufferSource = this.audioCtx.createBufferSource();
       const length = this.samples.length / this.option.channels;
       const audioBuffer = this.audioCtx.createBuffer(this.option.channels, length, this.option.sampleRate);
 
       for (let channel = 0; channel < this.option.channels; channel++) {
         const audioData = audioBuffer.getChannelData(channel);
         let offset = channel;
-        let decrement = 50;
+
         for (let i = 0; i < length; i++) {
           audioData[i] = this.samples[offset];
           offset += this.option.channels;
