022a067f66acbd7be08034263359ef86103e93e4
Michał Gapiński <mike@gapinski.eu>
2023-10-21 13:29:20 +0200

    Disable sentry

--- jenkins/multi-branch-ci.groovy
+++ jenkins/multi-branch-ci.groovy
@@ -56,14 +56,14 @@ pipeline {
                 sh('flutter build web --profile --web-renderer html --source-maps --dart-define=SENTRY_RELEASE=' + SENTRY_RELEASE)
             }
         }
-        stage('Upload debug symbols to Sentry') {
-            steps {
-                sh('sentry-cli releases new ' + SENTRY_RELEASE)
-                sh('sentry-cli releases files ' + SENTRY_RELEASE + ' upload-sourcemaps . --ext dart --dist 1')
-                sh('sentry-cli releases files ' + SENTRY_RELEASE + ' upload-sourcemaps "build/web" --ext map --ext js --dist 1')
-                sh('sentry-cli releases finalize ' + SENTRY_RELEASE)
-            }
-        }
+        //stage('Upload debug symbols to Sentry') {
+        //    steps {
+        //        sh('sentry-cli releases new ' + SENTRY_RELEASE)
+        //        sh('sentry-cli releases files ' + SENTRY_RELEASE + ' upload-sourcemaps . --ext dart --dist 1')
+        //        sh('sentry-cli releases files ' + SENTRY_RELEASE + ' upload-sourcemaps "build/web" --ext map --ext js --dist 1')
+        //        sh('sentry-cli releases finalize ' + SENTRY_RELEASE)
+        //    }
+        //}
         stage('Prepare artifacts') {
             steps {
                 sh('cd build/web && zip ' + SENTRY_RELEASE + '.zip -r *')
